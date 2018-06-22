e42f5d5be9b4c1dce827b989c0b6d00357fd6c51
timComp3215 <33968140+timComp3215@users.noreply.github.com>
2018-06-22 13:10:17 +0100

    Initial project upload
    Reads at 2 Hz
    Simply sends to UART in ASCII format after each read

---
+++ adc_timerA_uart/adc_timerA_uart.c
@@ -0,0 +1,241 @@
+/* --COPYRIGHT--,BSD
+ * Copyright (c) 2017, Texas Instruments Incorporated
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * *  Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * *  Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * *  Neither the name of Texas Instruments Incorporated nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * --/COPYRIGHT--*/
+/*******************************************************************************
+ * MSP432 ADC14 - Single Channel Continuous Sample w/ Timer_A Trigger
+ *
+ * Description: In this ADC14 code example, a single input channel is sampled
+ * using the standard 3.3v reference. The source of the sample trigger for this
+ * example is Timer_A CCR1. The ADC is setup to continuously sample/convert
+ * from A0 when the trigger starts and store the results in resultsBuffer (it
+ * is setup to be a circular buffer where resPos overflows to 0). Timer_A is
+ * setup in Up mode and a Compare value of 16384  is set as the compare trigger
+ *  and reset trigger. Once the Timer_A is started, after 0.5s it will trigger
+ * the ADC14 to start conversions. Essentially this example will use
+ * the Timer_A module to trigger an ADC conversion every 0.5 seconds.
+ *
+ *                MSP432P401
+ *             ------------------
+ *         /|\|                  |
+ *          | |                  |
+ *          --|RST         P5.5  |<--- A0 (Analog Input)
+ *            |                  |
+ *            |                  |
+ *            |                  |
+ *            |                  |
+ *            |                  |
+ *
+ *
+ * Edited version of example code to send ADC reading over serial in ASCII
+ * 9600 baud rate
+ *
+ ******************************************************************************/
+/* DriverLib Includes */
+#include <ti/devices/msp432p4xx/driverlib/driverlib.h>
+
+/* Standard Includes */
+#include <stdint.h>
+#include <stdbool.h>
+
+//![Simple UART Config]
+/* UART Configuration Parameter. These are the configuration parameters to
+ * make the eUSCI A UART module to operate with a 9600 baud rate. These
+ * values were calculated using the online calculator that TI provides
+ * at:
+ *http://software-dl.ti.com/msp430/msp430_public_sw/mcu/msp430/MSP430BaudRateConverter/index.html
+ */
+
+void sendReading(uint16_t reading)
+{
+    /*Convert reading to ASCII and send over serial */
+
+    uint8_t ascii_num[10] = {0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39};
+
+    UART_transmitData(EUSCI_A0_BASE, ascii_num[(reading / 10000)]);
+    UART_transmitData(EUSCI_A0_BASE, ascii_num[((reading % 10000)/1000)]);
+    UART_transmitData(EUSCI_A0_BASE, ascii_num[((reading % 1000)/100)]);
+    UART_transmitData(EUSCI_A0_BASE, ascii_num[((reading % 100)/10)]);
+    UART_transmitData(EUSCI_A0_BASE, ascii_num[(reading % 10)]);
+    UART_transmitData(EUSCI_A0_BASE, '\n');
+}
+
+//Configures to 9600 baud rate at this clock speed
+const eUSCI_UART_Config uartConfig =
+{
+        EUSCI_A_UART_CLOCKSOURCE_SMCLK,          // SMCLK Clock Source
+        78,                                      // BRDIV = 78
+        2,                                       // UCxBRF = 2
+        0,                                       // UCxBRS = 0
+        EUSCI_A_UART_NO_PARITY,                  // No Parity
+        EUSCI_A_UART_LSB_FIRST,                  // LSB First
+        EUSCI_A_UART_ONE_STOP_BIT,               // One stop bit
+        EUSCI_A_UART_MODE,                       // UART mode
+        EUSCI_A_UART_OVERSAMPLING_BAUDRATE_GENERATION  // Oversampling
+};
+
+/* Timer_A Continuous Mode Configuration Parameter */
+const Timer_A_UpModeConfig upModeConfig =
+{
+        TIMER_A_CLOCKSOURCE_ACLK,            // ACLK Clock Source
+        TIMER_A_CLOCKSOURCE_DIVIDER_1,       // ACLK/1 = 32.768Khz
+        16384,
+        TIMER_A_TAIE_INTERRUPT_DISABLE,      // Disable Timer ISR
+        TIMER_A_CCIE_CCR0_INTERRUPT_DISABLE, // Disable CCR0
+        TIMER_A_DO_CLEAR                     // Clear Counter
+};
+
+/* Timer_A Compare Configuration Parameter */
+const Timer_A_CompareModeConfig compareConfig =
+{
+        TIMER_A_CAPTURECOMPARE_REGISTER_1,          // Use CCR1
+        TIMER_A_CAPTURECOMPARE_INTERRUPT_DISABLE,   // Disable CCR interrupt
+        TIMER_A_OUTPUTMODE_SET_RESET,               // Toggle output but
+        16384                                       // 16000 Period
+};
+
+/* Statics */
+volatile uint16_t resultsBuffer[UINT8_MAX];
+volatile uint8_t resPos = 0;
+
+int main(void)
+{
+    /* Halting WDT  */
+    MAP_WDT_A_holdTimer();
+
+    /* Setting up clocks
+     * MCLK = MCLK = 12MHz
+     * ACLK = REFO = 32Khz */
+    /* Setting DCO to 12MHz */
+    CS_setDCOCenteredFrequency(CS_DCO_FREQUENCY_12);
+    CS_setReferenceOscillatorFrequency(CS_REFO_32KHZ);
+    MAP_CS_initClockSignal(CS_ACLK, CS_REFOCLK_SELECT, CS_CLOCK_DIVIDER_1);
+
+    //Set serial pins to serial mode
+    MAP_GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P1,
+                GPIO_PIN2 | GPIO_PIN3, GPIO_PRIMARY_MODULE_FUNCTION);
+
+    /* Configuring UART Module */
+    MAP_UART_initModule(EUSCI_A0_BASE, &uartConfig);
+
+    /* Enable UART module */
+    MAP_UART_enableModule(EUSCI_A0_BASE);
+
+    /* Enabling interrupts */
+    //MAP_UART_enableInterrupt(EUSCI_A0_BASE, EUSCI_A_UART_RECEIVE_INTERRUPT);
+    //MAP_Interrupt_enableInterrupt(INT_EUSCIA0);
+
+    /* Initializing ADC (MCLK/1/1) */
+    MAP_ADC14_enableModule();
+    MAP_ADC14_initModule(ADC_CLOCKSOURCE_MCLK, ADC_PREDIVIDER_1, ADC_DIVIDER_1,
+            0);
+
+    /* Configuring GPIOs (5.5 A0) */
+    MAP_GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P5, GPIO_PIN5,
+    GPIO_TERTIARY_MODULE_FUNCTION);
+
+    /* Configuring ADC Memory */
+    MAP_ADC14_configureSingleSampleMode(ADC_MEM0, true);
+    MAP_ADC14_configureConversionMemory(ADC_MEM0, ADC_VREFPOS_AVCC_VREFNEG_VSS,
+    ADC_INPUT_A0, false);
+
+    /* Configuring Timer_A in continuous mode and sourced from ACLK */
+    MAP_Timer_A_configureUpMode(TIMER_A0_BASE, &upModeConfig);
+
+    /* Configuring Timer_A0 in CCR1 to trigger at 16000 (0.5s) */
+    MAP_Timer_A_initCompare(TIMER_A0_BASE, &compareConfig);
+
+    /* Configuring the sample trigger to be sourced from Timer_A0  and setting it
+     * to automatic iteration after it is triggered*/
+    MAP_ADC14_setSampleHoldTrigger(ADC_TRIGGER_SOURCE1, false);
+
+    /* Enabling the interrupt when a conversion on channel 1 is complete and
+     * enabling conversions */
+    MAP_ADC14_enableInterrupt(ADC_INT0);
+    MAP_ADC14_enableConversion();
+
+    /* Enabling Interrupts */
+    MAP_Interrupt_enableInterrupt(INT_ADC14);
+    MAP_Interrupt_enableMaster();
+
+    /* Starting the Timer */
+    MAP_Timer_A_startCounter(TIMER_A0_BASE, TIMER_A_UP_MODE);
+
+    while (1)
+    {
+        //MAP_PCM_gotoLPM0();
+        if (resPos == 1)
+        {
+            int i;
+            for (i=0; i<1; i++)
+            {
+                sendReading(resultsBuffer[i]);
+            }
+            resPos = 0;
+        }
+    }
+}
+
+/* This interrupt is fired whenever a conversion is completed and placed in
+ * ADC_MEM0 */
+void ADC14_IRQHandler(void)
+{
+    uint64_t status;
+
+    status = MAP_ADC14_getEnabledInterruptStatus();
+    MAP_ADC14_clearInterruptFlag(status);
+
+
+    if (status & ADC_INT0)
+    {
+        if(resPos < 1)
+        {
+            resultsBuffer[resPos] = MAP_ADC14_getResult(ADC_MEM0);
+            resPos++;
+        }
+        
+
+    }
+
+}
+
+void EUSCIA0_IRQHandler(void)
+{
+   if (UCA0IFG & UCRXIFG)
+    {
+      while(!(UCA0IFG&UCTXIFG));
+//      asm(" nop");
+        //UCA0TXBUF = UCA0RXBUF;
+        uint8_t color = UCA0RXBUF;
+        UART_transmitData(EUSCI_A0_BASE, color);
+        UART_transmitData(EUSCI_A0_BASE, '\n');
+//      __no_operation();
+    }
+}
